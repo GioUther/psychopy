741f80915d8d653f2336c23d78614a9952bbee78
Jonas Kristoffer Lindeløv <jonas@id.aau.dk>
2019-04-03 23:31:46 +0200

    Updated tutorial1 to attributeSetter and PEP8

--- docs/source/coder/tutorial1.py
+++ docs/source/coder/tutorial1.py
@@ -1,23 +1,23 @@
-from psychopy import visual, core, event #import some libraries from PsychoPy
+from psychopy import visual, core, event  # Import some libraries from PsychoPy
 
-#create a window
-mywin = visual.Window([800,600],monitor="testMonitor", units="deg")
+# Create a window
+mywin = visual.Window([800, 600], monitor="testMonitor", units="deg")
 
-#create some stimuli
-grating = visual.GratingStim(win=mywin, mask='circle', size=3, pos=[-4,0], sf=3)
-fixation = visual.GratingStim(win=mywin, size=0.2, pos=[0,0], sf=0, rgb=-1)
+# Create some stimuli
+grating = visual.GratingStim(win=mywin, mask='circle', size=3, pos=[-4, 0], sf=3)
+fixation = visual.GratingStim(win=mywin, size=0.2, pos=[0, 0], sf=0, rgb=-1)
 
-#draw the stimuli and update the window
-while True: #this creates a never-ending loop
-    grating.setPhase(0.05, '+')#advance phase by 0.05 of a cycle
+# Draw the stimuli and update the window
+while True:  # This creates a never-ending loop
+    grating.phase += 0.05  # Advance phase by 0.05 of a cycle
     grating.draw()
     fixation.draw()
     mywin.flip()
 
-    if len(event.getKeys())>0:
+    if len(event.getKeys()) > 0:
         break
     event.clearEvents()
 
-#cleanup
+# Cleanup
 mywin.close()
 core.quit()
